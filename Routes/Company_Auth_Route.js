dc2fb95ffe462a9f95d71687b74aa5ad245f0e74
basma bs <bassmaboussaidi@gmail.com>
2022-11-21 12:31:52 +0100

    update passport

--- Routes/Company_Auth_Route.js
+++ Routes/Company_Auth_Route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const passport = require('passport');
 const { Register, Login, forgetPassword, resetPassword } = require('../Controllers/Company_Auth_Controller');
 router = express.Router();
 router.post('/register', Register);
